2c4473db384c4c6e2f3b35b578d58473b95df05b
PL <philip.lundin@iths.se>
2021-05-10 10:10:54 +0200

    updated stats menu

--- src/main/java/menu/StatisticsMenu.java
+++ src/main/java/menu/StatisticsMenu.java
@@ -9,9 +9,9 @@ public class StatisticsMenu {
 
         System.out.println("\nSTATISTICS MENU");
         System.out.println("---------------");
-        System.out.println("1. ");
-        System.out.println("2. ");
-        System.out.println("3. ");
+        System.out.println("1. Gender Ratio");
+        System.out.println("2. Average Age");
+        System.out.println("3. Average Grade");
 
         System.out.println("\n6. Return");
         System.out.println("8. Main Menu");
@@ -24,13 +24,13 @@ public class StatisticsMenu {
         Integer choice = Input.inputInt();
         switch (choice) {
             case 1:
-                //TODO update menu, switch cases and add calls
+                //TODO call genderRatio
                 break;
             case 2:
-
+                //TODO call averageAge
                 break;
             case 3:
-
+                //TODO call averageGrade
                 break;
             case 6:
                 return;
