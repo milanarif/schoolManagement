8c70dbc135c9cc3cfec030c936116cc871cbd1c3
PL <philip.lundin@iths.se>
2021-05-10 13:24:17 +0200

    updated statsmenu

--- src/main/java/menu/StatisticsMenu.java
+++ src/main/java/menu/StatisticsMenu.java
@@ -9,11 +9,11 @@ public class StatisticsMenu {
 
         System.out.println("\nSTATISTICS MENU");
         System.out.println("---------------");
-        System.out.println("1. Number of Students");
-        System.out.println("2. Number of Teachers");
-        System.out.println("3. Gender Ratio");
-        System.out.println("4. Average Age");
-        System.out.println("5. Average Grade");
+        System.out.println("1. Pass ratio");
+        System.out.println("2. Gender Ratio");
+        System.out.println("3. Average Age");
+        System.out.println("4. Number of Students");
+        System.out.println("5. Number of Teachers");
 
         System.out.println("\n6. Return");
         System.out.println("8. Main Menu");
