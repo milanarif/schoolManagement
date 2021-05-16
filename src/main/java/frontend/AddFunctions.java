bce279af2f6338b9ae1129dcc086cc4b1f00a3ae
PL <philip.lundin@iths.se>
2021-05-16 19:22:51 +0200

    fixed if-statements

--- src/main/java/frontend/AddFunctions.java
+++ src/main/java/frontend/AddFunctions.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class AddFunctions {
 
-    public static void addCourse(){
+    public static void addCourse() {
 
         System.out.print("Course name: ");
         String name = Input.inputString();
@@ -17,12 +17,21 @@ public class AddFunctions {
         System.out.print("Credits: ");
         Double credits = Input.inputDouble();
 
-        Course course = new Course(name, credits);
-
-        CourseFunctions.addCourse(course);
+        boolean uniqueCourse = false;
+        List<Course> courses = CourseFunctions.getAllCourses();
+        for (Course c : courses) {
+            uniqueCourse = c.getName().equals(name);
+        }
+        if (uniqueCourse) {
+            System.out.println("\nCourse by that name already exists!");
+        } else {
+            System.out.println("\nCourse " + name + " Successfully Added!");
+            Course course = new Course(name, credits);
+            CourseFunctions.addCourse(course);
+        }
     }
 
-    public static void addStudent(){
+    public static void addStudent() {
         System.out.print("Student SSN (YYYYMMDD-XXXX): ");
         String socialSecurity = Input.inputSSN();
 
@@ -34,13 +43,14 @@ public class AddFunctions {
         Student student = new Student(socialSecurity, name, gender);
 
         if (StudentFunctions.getAllStudents().contains(student)) {
-            System.out.println("Student with that social security already exists. (" + socialSecurity + ").");
+            System.out.println("\nStudent with that social security already exists " + socialSecurity + "!");
         } else {
+            System.out.println("\nStudent " + socialSecurity + ", " + name + " Successfully Added");
             StudentFunctions.addStudent(student);
         }
     }
 
-    public static void addTeacher(){
+    public static void addTeacher() {
         System.out.print("Teacher SSN (YYYYMMDD-XXXX): ");
         String socialSecurity = Input.inputSSN();
 
@@ -52,26 +62,28 @@ public class AddFunctions {
         Teacher teacher = new Teacher(socialSecurity, name, gender);
 
         if (TeacherFunctions.getAllTeachers().contains(teacher)) {
-            System.out.println("Teacher with that social security already exists. (" + socialSecurity + ").");
+            System.out.println("Teacher with that social security already exists " + socialSecurity + "!");
         } else {
+            System.out.println("\nTeacher " + socialSecurity + ", " + name + " Successfully Added");
             TeacherFunctions.addTeacher(teacher);
         }
     }
 
-    public static void addDepartment(){
+    public static void addDepartment() {
 
         System.out.print("Department name: ");
         String name = Input.inputString();
-        boolean unique = true;
-        Department department = new Department(name);
+
+        boolean uniqueDepartment = false;
         List<Department> departments = DepartmentFunctions.getAllDepartments();
         for (Department d : departments) {
-            if (d.getName() == name) {
-                System.out.println("Department with this name already exists");
-                unique = false;
-            }
+            uniqueDepartment = d.getName().equals(name);
         }
-        if (unique) {
+        if (uniqueDepartment) {
+            System.out.println("\nDepartment by that name already exists!");
+        } else {
+            System.out.println("\nDepartment " + name + " Successfully Added!");
+            Department department = new Department(name);
             DepartmentFunctions.addDepartment(department);
         }
     }
