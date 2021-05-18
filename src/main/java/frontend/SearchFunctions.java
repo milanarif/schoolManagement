f52339daf461ee1ce4cb6c10acbf4fba548ef256
Shorl <milanarif@gmail.com>
2021-05-18 16:31:23 +0200

    various fixes

--- src/main/java/frontend/SearchFunctions.java
+++ src/main/java/frontend/SearchFunctions.java
@@ -9,111 +9,123 @@ import java.util.List;
 public class SearchFunctions {
 
     public static void searchCourse() {
+        if (CourseFunctions.getAllCourses() != null && !CourseFunctions.getAllCourses().isEmpty()) {
+            System.out.print("Course id: ");
+            Integer courseId = Input.inputInt();
 
-        System.out.print("Course id: ");
-        Integer courseId = Input.inputInt();
+            Course course = CourseFunctions.getCourse(courseId);
 
-        Course course = CourseFunctions.getCourse(courseId);
-
-        if(course != null) {
-            List<Enrollment> enrollment = CourseFunctions.getCourse(courseId).getEnrollments();
-            String courseName = null;
-            List<Teacher> teacherName1;
-            String teacherName = null;
-            for (Enrollment e:enrollment) {
-                courseName = e.getCourse().getName();
-                teacherName1 = e.getCourse().getTeachers();
-                for (Teacher t:teacherName1) {
-                    teacherName = t.getName();
+            if(course != null) {
+                List<Enrollment> enrollment = CourseFunctions.getCourse(courseId).getEnrollments();
+                String courseName = null;
+                List<Teacher> teacherName1;
+                String teacherName = null;
+                for (Enrollment e:enrollment) {
+                    courseName = e.getCourse().getName();
+                    teacherName1 = e.getCourse().getTeachers();
+                    for (Teacher t:teacherName1) {
+                        teacherName = t.getName();
+                    }
                 }
-            }
-            System.out.println("\nCourse: " + courseName);
-            System.out.println("Teacher: " + teacherName);
-            System.out.println("\n" + enrollment.size() + " Students in course ");
-            System.out.println("-------------------");
+                System.out.println("\nCourse: " + courseName);
+                System.out.println("Teacher: " + teacherName);
+                System.out.println("\n" + enrollment.size() + " Students in course ");
+                System.out.println("-------------------");
 
-            for (Enrollment e:enrollment) {
-                System.out.println(e.getStudent().getName());
+                for (Enrollment e:enrollment) {
+                    System.out.println(e.getStudent().getName());
+                }
+            } else {
+                System.out.println("\nNo such course ID " + courseId+"");
             }
-        } else {
-            System.out.println("\nNo such course ID " + courseId+"");
+            } else {
+            System.out.println("No courses in database.");
+            }
         }
-    }
 
     public static void searchStudent() {
+        if (StudentFunctions.getAllStudents() != null && !StudentFunctions.getAllStudents().isEmpty()) {
+            System.out.print("Student SSN (YYYYMMDD-XXXX): ");
+            String socialSecurity = Input.inputSSN();
 
-        System.out.print("Student SSN (YYYYMMDD-XXXX): ");
-        String socialSecurity = Input.inputSSN();
+            Student student = StudentFunctions.getStudent(socialSecurity);
 
-        Student student = StudentFunctions.getStudent(socialSecurity);
+            if (student != null) {
+                List<Enrollment> enrollments = StudentFunctions.getStudent(socialSecurity).getEnrollments();
 
-        if(student != null){
-            List<Enrollment> enrollments = StudentFunctions.getStudent(socialSecurity).getEnrollments();
+                System.out.println("\n" + student.getName());
+                System.out.println(socialSecurity);
+                System.out.println("\nIn " + enrollments.size() + " courses");
+                System.out.println("--------------");
 
-            System.out.println("\n" + student.getName());
-            System.out.println(socialSecurity);
-            System.out.println("\nIn " + enrollments.size() + " courses");
-            System.out.println("--------------");
-
-            for (Enrollment e:enrollments) {
-                if (e.getCourse() != null) {
-                    System.out.println(e.getCourse().getName());
-                } else {
-                    System.out.println("Expired Course.");
+                for (Enrollment e : enrollments) {
+                    if (e.getCourse() != null) {
+                        System.out.println(e.getCourse().getName());
+                    } else {
+                        System.out.println("Expired Course.");
+                    }
                 }
+            } else {
+                System.out.println("\nNo such student " + socialSecurity + "!");
             }
         } else {
-            System.out.println("\nNo such student " + socialSecurity + "!");
+            System.out.println("No students in database.");
         }
     }
 
     public static void searchTeacher() {
+        if (TeacherFunctions.getAllTeachers() != null && !TeacherFunctions.getAllTeachers().isEmpty()) {
+            System.out.print("Teacher SSN (YYYYMMDD-XXXX): ");
+            String socialSecurity = Input.inputSSN();
 
-        System.out.print("Teacher SSN (YYYYMMDD-XXXX): ");
-        String socialSecurity = Input.inputSSN();
+            Teacher teacher = TeacherFunctions.getTeacher(socialSecurity);
 
-        Teacher teacher = TeacherFunctions.getTeacher(socialSecurity);
+            if (teacher != null) {
+                List<Course> courses = TeacherFunctions.getTeacher(socialSecurity).getCourses();
 
-        if(teacher != null){
-            List<Course> courses = TeacherFunctions.getTeacher(socialSecurity).getCourses();
+                System.out.println("\n" + teacher.getName());
+                System.out.println(socialSecurity);
+                System.out.println("\nIn " + courses.size() + " courses");
+                System.out.println("--------------");
 
-            System.out.println("\n" + teacher.getName());
-            System.out.println(socialSecurity);
-            System.out.println("\nIn " + courses.size() + " courses");
-            System.out.println("--------------");
-
-            for (Course c:courses) {
-                System.out.println(c.getName());
+                for (Course c : courses) {
+                    System.out.println(c.getName());
+                }
+            } else {
+                System.out.println("\nNo such teacher " + socialSecurity + "!");
             }
         } else {
-            System.out.println("\nNo such teacher " + socialSecurity + "!");
+            System.out.println("\nNo teachers in database.");
         }
     }
 
     public static void searchDepartment() {
+        if (DepartmentFunctions.getAllDepartments() != null && !DepartmentFunctions.getAllDepartments().isEmpty()) {
+            System.out.print("Department ID: ");
+            Integer departmentId = Input.inputInt();
 
-        System.out.print("Department ID: ");
-        Integer departmentId = Input.inputInt();
+            Department department = DepartmentFunctions.getDepartment(departmentId);
 
-        Department department = DepartmentFunctions.getDepartment(departmentId);
+            if(department != null) {
+                List<Student> students = DepartmentFunctions.getDepartment(departmentId).getStudents();
+                List<Teacher> teachers = DepartmentFunctions.getDepartment(departmentId).getTeachers();
 
-        if(department != null) {
-            List<Student> students = DepartmentFunctions.getDepartment(departmentId).getStudents();
-            List<Teacher> teachers = DepartmentFunctions.getDepartment(departmentId).getTeachers();
+                Integer totalSize = students.size() + teachers.size();
+                System.out.println("\nDepartment " + department.getName());
+                System.out.println("Total " + totalSize + " Persons");
+                System.out.println("-------------------");
 
-            Integer totalSize = students.size() + teachers.size();
-            System.out.println("\nDepartment " + department.getName());
-            System.out.println("Total " + totalSize + " Persons");
-            System.out.println("-------------------");
-
-            for (Student s: students){
-                System.out.println(s.getName());
-            }
-            for (Teacher t : teachers){
-                System.out.println(t.getName());
+                for (Student s: students){
+                    System.out.println(s.getName());
+                }
+                for (Teacher t : teachers){
+                    System.out.println(t.getName());
+                }
+            } else {
+                System.out.println("No such department ID " + departmentId+"!");
             }
         } else {
-            System.out.println("No such department ID " + departmentId+"!");
+            System.out.println("No departments in database.");
         }
     }
 }
