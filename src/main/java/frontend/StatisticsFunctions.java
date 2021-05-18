43843a0f35bc0f546a213384d2862e519577e1d8
PL <philip.lundin@iths.se>
2021-05-18 14:19:16 +0200

    moved code from menu to frotend

--- src/main/java/frontend/StatisticsFunctions.java
+++ src/main/java/frontend/StatisticsFunctions.java
@@ -1,58 +1,135 @@
 
 package frontend;
 
+import backend.CourseFunctions;
+import backend.DepartmentFunctions;
 import backend.StudentFunctions;
 import backend.TeacherFunctions;
-import entity.Enrollment;
-import entity.Student;
-import entity.Teacher;
+import entity.*;
+import main.Input;
 
 import java.util.List;
 
 public class StatisticsFunctions {
 
-    public static Integer passRatio(List<Enrollment> enrollments) {
-        if (enrollments == null) {
-            return null;
-        }
-        int totalEnrollments = enrollments.size();
-        int passes = 0;
-        for (Enrollment e : enrollments) {
-            if (e.getGrade() != null) {
-                if (e.getGrade() > 2) {
-                    passes++;
+    public static void passRatioCourse() {
+
+        System.out.print("Course ID: ");
+        Integer courseId = Input.inputInt();
+        Course course = CourseFunctions.getCourse(courseId);
+
+        if (course != null) {
+            List<Enrollment> enrollments = course.getEnrollments();
+
+            int totalEnrollments = enrollments.size();
+            int passes = 0;
+            for (Enrollment e : enrollments) {
+                if (e.getGrade() != null) {
+                    if (e.getGrade() > 2) {
+                        passes++;
+                    }
                 }
             }
+            System.out.println("\nPASSRATIO");
+            System.out.println("COURSE " + courseId);
+            System.out.println("------------");
+            System.out.println((passes / totalEnrollments) * 100 + "% passed");
+        } else {
+            System.out.println("\nCourse " + courseId + " Not Found!");
         }
-        return ((passes/totalEnrollments)*100);
     }
 
-    public static Integer genderRatioCourse(List<Enrollment> enrollments) {
-        if (enrollments == null) {
-            return null;
-        }
-        int totalEnrollments = enrollments.size();
-        int female = 0;
-        for (Enrollment e : enrollments) {
-            if (e.getStudent().getGender() == "Female") {
-                female++;
+    public static void passRatioStudent() {
+
+        System.out.print("Student SSN (YYYYMMDD-XXXX): ");
+        String socialSecurity = Input.inputSSN();
+        Student student = StudentFunctions.getStudent(socialSecurity);
+
+        if (student != null) {
+            List<Enrollment> enrollments = student.getEnrollments();
+
+            if (enrollments != null) {
+
+                int totalEnrollments = enrollments.size();
+                int passes = 0;
+                for (Enrollment e : enrollments) {
+                    if (e.getGrade() != null) {
+                        if (e.getGrade() > 2) {
+                            passes++;
+                        }
+                    }
+                }
+                //TODO not working 100% correctly
+                if (passes > 0) {
+                    System.out.println("\nPASSRATIO");
+                    System.out.println("STUDENT " + student.getName());
+                    System.out.println("------------");
+                    System.out.println("Courses passed " + (passes / totalEnrollments) * 100 + "%");
+                } else {
+                    System.out.println("\nEnrollment Not Found!");
+                }
+            } else {
+                System.out.println("\n Student " + socialSecurity + " Not Found!");
             }
         }
-        return ((female/totalEnrollments)*100);
     }
 
-    public static Integer genderRatioDepartment(List<Student> students) {
-        if (students == null) {
-            return null;
-        }
-        int totalStudents = students.size();
-        int female = 0;
-        for (Student s : students) {
-            if (s.getGender() == "Female") {
-                female++;
+    public static void genderRatioCourse() {
+
+        System.out.print("Course ID: ");
+        Integer courseId = Input.inputInt();
+
+        Course courseGender = CourseFunctions.getCourse(courseId);
+
+        if(courseGender !=null) {
+            List<Enrollment> enrollments = courseGender.getEnrollments();
+
+            if (enrollments != null) {
+                int totalEnrollments = enrollments.size();
+                int female = 0;
+                for (Enrollment e : enrollments) {
+                    if (e.getStudent().getGender() == "Female") {
+                        female++;
+                    }
+                }
+                //TODO somethings wrong with female count + improve print
+                System.out.println((female/totalEnrollments)*100 + " %");
             }
+            else {
+                System.out.println("\nEnrollment Not Found!");
+            }
+        } else {
+            System.out.println("\nCourse " + courseId + " Not Found!");
         }
-        return ((female/totalStudents)*100);
+    }
+
+    public static void genderRatioDepartment() {
+
+        System.out.print("Department ID: ");
+        Integer departmentId = Input.inputInt();
+
+        Department department = DepartmentFunctions.getDepartment(departmentId);
+        if(department != null) {
+            List<Student> students = department.getStudents();
+
+            if (students != null) {
+                int totalStudents = students.size();
+                int female = 0;
+                for (Student s : students) {
+                    if (s.getGender() == "Female") {
+                        female++;
+                    }
+                }
+
+                //TODO not working correctly + improve print
+                System.out.println((female / totalStudents) * 100 + "%");
+
+            }else {
+                System.out.println("\nNot Found");
+            }
+        } else {
+            System.out.println("\nDepartment " + departmentId + " Not Found!");
+        }
     }
 
     public static void numberOfPerson() {
@@ -71,4 +148,6 @@ public class StatisticsFunctions {
         System.out.printf("%-10d", total);
         System.out.println(" ");
     }
+
+
 }
--- src/main/java/menu/StatisticsMenu.java
+++ src/main/java/menu/StatisticsMenu.java
@@ -1,11 +1,5 @@
 package menu;
 
-import backend.CourseFunctions;
-import backend.DepartmentFunctions;
-import backend.StudentFunctions;
-import entity.Course;
-import entity.Department;
-import entity.Student;
 import frontend.StatisticsFunctions;
 import main.Main;
 import main.Input;
@@ -29,50 +23,21 @@ public class StatisticsMenu {
 
         System.out.print("choice: ");
         Integer choice = Input.inputInt();
-        Integer courseId;
-        String socialSecurity;
-        Integer departmentId;
         switch (choice) {
             case 1:
-                System.out.print("Course ID: ");
-                courseId = Input.inputInt();
-                Course course = CourseFunctions.getCourse(courseId);
-                if(course != null)
-                    StatisticsFunctions.passRatio(course.getEnrollments());
-                else
-                    System.out.println("\nCourse (" + courseId + ") Not Found!");
+                StatisticsFunctions.passRatioCourse();
                 menu();
                 break;
             case 2:
-                System.out.print("Student SSN (YYYYMMDD-XXXX): ");
-                socialSecurity = Input.inputString();
-                Student student = StudentFunctions.getStudent(socialSecurity);
-                if(student != null)
-                    StatisticsFunctions.passRatio(student.getEnrollments());
-                else
-                    System.out.println("\n Student (" + socialSecurity + ") Not Found!");
+                StatisticsFunctions.passRatioStudent();
                 menu();
                 break;
             case 3:
-                System.out.print("Course ID: ");
-                courseId = Input.inputInt();
-
-                Course courseGender = CourseFunctions.getCourse(courseId);
-                if(courseGender !=null)
-                    StatisticsFunctions.genderRatioCourse(courseGender.getEnrollments());
-                else
-                    System.out.println("\nCourse (" + courseId + ") Not Found!");
+                StatisticsFunctions.genderRatioCourse();
                 menu();
                 break;
             case 4:
-                System.out.print("Department ID: ");
-                departmentId = Input.inputInt();
-
-                Department department = DepartmentFunctions.getDepartment(departmentId);
-                if(department != null)
-                    StatisticsFunctions.genderRatioDepartment(department.getStudents());
-                else
-                    System.out.println("\nDepartment (" + departmentId +") Not Found!");
+                StatisticsFunctions.genderRatioDepartment();
                 menu();
                 break;
             case 5:
